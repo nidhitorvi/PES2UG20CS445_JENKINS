75155b0cf3757262fe32f246f151e56e9eae61e1
nidhitorvi <115217165+nidhitorvi@users.noreply.github.com>
2023-02-19 10:30:25 -0500

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and i m nidhi \n";
+  return 0;
+}
